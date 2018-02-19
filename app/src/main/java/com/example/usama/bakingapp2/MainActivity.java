89b0e723f9dcc0d0f9a92e734306bb58a8b0a381
usama <usamanadeem1991@gmail.com>
2018-02-20 01:00:50 +0500

    created recyclerview for recipes

--- app/src/main/java/com/example/usama/bakingapp2/MainActivity.java
+++ app/src/main/java/com/example/usama/bakingapp2/MainActivity.java
@@ -2,11 +2,21 @@ package com.example.usama.bakingapp2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.main_activity_recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
 
 
     }
@@ -37,4 +53,40 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public class RecyclerAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
+
+
+        public String[] recipes = {"Recipe One", "Recipe Two", "Recipe 3"};
+
+        @Override
+        public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recipe_card_layout, parent, false);
+            RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(RecipeViewHolder holder, int position) {
+            holder.recipeNameTextView.setText(recipes[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return recipes.length;
+        }
+
+    }
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView recipeNameTextView;
+
+       public RecipeViewHolder(View itemView) {
+            super(itemView);
+            recipeNameTextView = itemView.findViewById(R.id.recipe_name_text_view);
+        }
+    }
+
+
 }

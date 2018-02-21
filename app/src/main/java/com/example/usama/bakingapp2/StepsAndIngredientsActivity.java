d6e651b9b2a0b26e401e8f8c5d852f625441901f
usama <usamanadeem1991@gmail.com>
2018-02-21 20:08:35 +0500

    created bottom navigation

--- app/src/main/java/com/example/usama/bakingapp2/StepsAndIngredientsActivity.java
+++ app/src/main/java/com/example/usama/bakingapp2/StepsAndIngredientsActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
 public class StepsAndIngredientsActivity extends RootActivity {
 
@@ -16,6 +20,26 @@ public class StepsAndIngredientsActivity extends RootActivity {
         setContentView(R.layout.activity_steps_and_ingredients);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+
+        AHBottomNavigationItem steps = new AHBottomNavigationItem(R.string.steps, R.drawable.ic_stairs, R.color.colorAccent);
+        AHBottomNavigationItem ingredients = new AHBottomNavigationItem(R.string.ingredients, R.drawable.ic_harvest, R.color.colorAccent);
+
+        bottomNavigation.addItem(steps);
+        bottomNavigation.addItem(ingredients);
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                if (position == 0) {
+                    Toast.makeText(getApplicationContext(), "Steps", Toast.LENGTH_SHORT).show();
+                } else if (position == 1) {
+                    Toast.makeText(getApplicationContext(), "Ingredients", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +51,6 @@ public class StepsAndIngredientsActivity extends RootActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 
 
 }

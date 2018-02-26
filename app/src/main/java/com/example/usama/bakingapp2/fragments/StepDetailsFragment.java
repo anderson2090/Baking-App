ddb603559f309c263ac30b8952614c357d7d48f7
usama <usamanadeem1991@gmail.com>
2018-02-26 15:24:00 +0500

    created video player

--- app/src/main/java/com/example/usama/bakingapp2/fragments/StepDetailsFragment.java
+++ app/src/main/java/com/example/usama/bakingapp2/fragments/StepDetailsFragment.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.usama.bakingapp2.BakingApp;
 import com.example.usama.bakingapp2.R;
 import com.example.usama.bakingapp2.model.Step;
 import com.google.android.exoplayer2.DefaultLoadControl;
@@ -43,23 +42,31 @@ import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 import com.google.android.exoplayer2.video.VideoRendererEventListener;
 import com.squareup.picasso.Picasso;
 
+import static android.content.ContentValues.TAG;
+
 public class StepDetailsFragment extends Fragment implements VideoRendererEventListener {
 
-    private static final String TAG = "MainActivity";
-    private SimpleExoPlayerView simpleExoPlayerView;
-    private SimpleExoPlayer player;
-    private TextView resolutionTextView;
+    SimpleExoPlayerView playerView;
+    int currentWindow;
+    long playbackPosition;
+    boolean playWhenReady = true;
+    SimpleExoPlayer player;
+    Step currentStep;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.step_details_fragment, container, false);
 
-        Step currentStep = getArguments().getParcelable("currentStep");
+        currentStep = getArguments().getParcelable("currentStep");
+
+        playerView = view.findViewById(R.id.player_view);
 
         TextView videoAvailableTextView = view.findViewById(R.id.video_available_text_view);
         TextView thumbNailAvailableTextView = view.findViewById(R.id.step_thumbnail_text_view);
@@ -90,27 +97,21 @@ public class StepDetailsFragment extends Fragment implements VideoRendererEventL
         descriptionTextView.setText(currentStep.getDescription());
 
 
+        return view;
+    }
+
+    private void initializePlayer() {
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
         TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
 
-
         LoadControl loadControl = new DefaultLoadControl();
 
-
         player = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
-        simpleExoPlayerView = new SimpleExoPlayerView(getActivity());
-        simpleExoPlayerView = (SimpleExoPlayerView) view.findViewById(R.id.player_view);
-
-
-        simpleExoPlayerView.setUseController(true);
-        simpleExoPlayerView.requestFocus();
-
-
-        simpleExoPlayerView.setPlayer(player);
-
+        playerView.setUseController(true);
+        playerView.requestFocus();
+        playerView.setPlayer(player);
         Uri mp4VideoUri =Uri.parse(currentStep.getVideoURL());
-
 
         DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
 
@@ -118,12 +119,9 @@ public class StepDetailsFragment extends Fragment implements VideoRendererEventL
 
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
 
-        MediaSource videoSource = new ExtractorMediaSource(mp4VideoUri, dataSourceFactory, extractorsFactory, null, null);
-
        // MediaSource videoSource = new HlsMediaSource(mp4VideoUri, dataSourceFactory, 1, null, null);
+        MediaSource videoSource = new ExtractorMediaSource(mp4VideoUri, dataSourceFactory, extractorsFactory, null, null);
         final LoopingMediaSource loopingSource = new LoopingMediaSource(videoSource);
-
-// Prepare the player with the source.
         player.prepare(loopingSource);
 
         player.addListener(new ExoPlayer.EventListener() {
@@ -139,7 +137,7 @@ public class StepDetailsFragment extends Fragment implements VideoRendererEventL
 
             @Override
             public void onLoadingChanged(boolean isLoading) {
-                Log.v(TAG, "Listener-onLoadingChanged...isLoading:" + isLoading);
+                Log.v(TAG, "Listener-onLoadingChanged...isLoading:"+isLoading);
             }
 
             @Override
@@ -149,9 +147,8 @@ public class StepDetailsFragment extends Fragment implements VideoRendererEventL
 
             @Override
             public void onRepeatModeChanged(int repeatMode) {
-
+                Log.v(TAG, "Listener-onRepeatModeChanged...");
             }
-
 
             @Override
             public void onPlayerError(ExoPlaybackException error) {
@@ -175,10 +172,56 @@ public class StepDetailsFragment extends Fragment implements VideoRendererEventL
         player.setPlayWhenReady(true);
         player.setVideoDebugListener(this);
 
-
-        return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 
     @Override
     public void onVideoEnabled(DecoderCounters counters) {
@@ -218,6 +261,6 @@ public class StepDetailsFragment extends Fragment implements VideoRendererEventL
     @Override
     public void onDestroy() {
         super.onDestroy();
-        player.release();
+        releasePlayer();
     }
 }
